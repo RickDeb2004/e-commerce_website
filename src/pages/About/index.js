a92e15da8131f4090e2a0665997fca9fafc6114d
RickDeb2004 <debanjanrick04@gmail.com>
2024-02-09 22:57:39 +0530

    feat: added

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,63 +1,74 @@
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import Debjan from './Debanjan.jpeg'
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import DebanjanImage from "../../assets/images/Debanjan.jpg";
+
 function About() {
   return (
-    <CardGroup>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>Card title</Card.Title>
-          <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardGroup>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card>
+          <CardMedia
+            component="img"
+            height="200px"
+            src={DebanjanImage}
+            alt="Debanjan"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Debanjan Mukherjee
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card>
+          <CardMedia
+            component="img"
+            height="200"
+            image={require("../../assets/images/Rishi_Mondal.jpg").default}
+            alt="Rishi Mondal"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Rishi Mondal
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <Card>
+          <CardMedia
+            component="img"
+            height="200"
+            image={require("../../assets/images/Rohit.jpg").default}
+            alt="Rohit"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Rohit
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              This card has supporting text below as a natural lead-in to
+              additional content.
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
