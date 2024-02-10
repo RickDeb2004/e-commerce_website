61f9fb4eb3d1915dc9fdbe2d70f5bb2daf2bc8c6
RickDeb2004 <debanjanrick04@gmail.com>
2024-02-10 13:26:25 +0530

    feat: added about us

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -5,65 +5,86 @@ import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import DebanjanImage from "../../assets/images/Debanjan.jpg";
+import RishiImage from "../../assets/images/Rishi_Mondal.jpg";
+import RohitImage from "../../assets/images/Rohit.jpg";
+import AnimeshImage from "../../assets/images/Animesh.jpeg";
+import "./style.css"; // Import CSS file for styling
 
 function About() {
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card className="card">
           <CardMedia
             component="img"
-            height="200px"
-            src={DebanjanImage}
+            className="cardMedia"
+            height="800"
+            image={DebanjanImage}
             alt="Debanjan"
           />
-          <CardContent>
+          <CardContent className="cardContent">
             <Typography gutterBottom variant="h5" component="div">
               Debanjan Mukherjee
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+             Font-end Developer
             </Typography>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card className="card">
           <CardMedia
             component="img"
-            height="200"
-            image={require("../../assets/images/Rishi_Mondal.jpg").default}
+            className="cardMedia"
+            height="800"
+            image={RishiImage}
             alt="Rishi Mondal"
           />
-          <CardContent>
+          <CardContent className="cardContent">
             <Typography gutterBottom variant="h5" component="div">
               Rishi Mondal
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
+             Full-stack Developer
             </Typography>
           </CardContent>
         </Card>
       </Grid>
-      <Grid item xs={12} sm={6} md={4}>
-        <Card>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card className="card">
           <CardMedia
             component="img"
-            height="200"
-            image={require("../../assets/images/Rohit.jpg").default}
+            className="cardMedia"
+            height="800"
+            image={RohitImage}
             alt="Rohit"
           />
-          <CardContent>
+          <CardContent className="cardContent">
             <Typography gutterBottom variant="h5" component="div">
               Rohit
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              This card has supporting text below as a natural lead-in to
-              additional content.
+             Backend Developer
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+      <Grid item xs={12} sm={6} md={3}>
+        <Card className="card">
+          <CardMedia
+            component="img"
+            className="cardMedia"
+            height="800"
+            image={AnimeshImage}
+            alt=" Animesh"
+          />
+          <CardContent className="cardContent">
+            <Typography gutterBottom variant="h5" component="div">
+              Animesh
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Front-end Developer
             </Typography>
           </CardContent>
         </Card>
